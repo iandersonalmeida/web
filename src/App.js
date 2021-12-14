a4936cf246812f21af3b521b03c6be2225bf669f
iandersonalmeida <90912680+iandersonalmeida@users.noreply.github.com>
2021-12-14 15:05:44 -0300

    Modify App.js and Add new file: FooterComponent.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
     <div>
-      <HeaderComponent></HeaderComponent>      
+      <HeaderComponent></HeaderComponent>  
+      <div>
+
+      </div>  
+      <FooterComponent></FooterComponent>
     </div>
   );
 }
