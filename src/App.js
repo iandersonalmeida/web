71e72a0ec8e2846d0ca6b86e0f2aff2996cd469e
iandersonalmeida <90912680+iandersonalmeida@users.noreply.github.com>
2021-12-14 15:28:03 -0300

    Modify index.html, App.js and MainContent.jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
+import MainContent from './components/MainContent';
 
 function App() {
   return (
     <div>
       <HeaderComponent></HeaderComponent>  
       <div>
+        <Router>
+          <Routes>
+            <Route path = "/"  element = {<MainContent/>}> /</Route>
+          </Routes>
+        </Router>
+        
 
       </div>  
       <FooterComponent></FooterComponent>
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class MainContent extends Component {
+    render() {
+        return (
+            <div className="container" >  
+                        
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <br/>  
+
+                <div className="d-flex justify-content-center">                                         
+                
+                <p > I'm Systems Analyst.
+                    <p className="text-muted">
+                         I am interested in systems development,
+                         developing ideas and suggestions<br/>
+                         for ways that IT can support and improve business process,<br/>
+                         helping to design new business process and information systems. </p>   
+
+                </p>
+               </div>  
+                    
+            </div>    
+                
+        );
+    }
+}
+
+export default MainContent;
