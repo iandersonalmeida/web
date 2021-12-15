42f047dad08f704eca6199e07cc2b05b9b7d2d38
iandersonalmeida <90912680+iandersonalmeida@users.noreply.github.com>
2021-12-15 14:39:20 -0300

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {HashRouter as Router, Routes, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import MainContent from './components/MainContent';
