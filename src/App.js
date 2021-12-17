4c7c3a729ee352ed280a324d207e0d27a1101120
iandersonalmeida <90912680+iandersonalmeida@users.noreply.github.com>
2021-12-17 15:20:44 -0300

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return (
     <div>
       <HeaderComponent></HeaderComponent>  
-      <MainContent></MainContent>
+      <MainContent/>
      
       <div>      
         <Router basename='/web'>
