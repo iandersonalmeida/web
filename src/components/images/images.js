d6fbd4a0030a75ea5ba7540968c6e0400679e1cf
iandersonalmeida <90912680+iandersonalmeida@users.noreply.github.com>
2021-12-18 09:27:32 -0300

    Add new files: images

---
+++ src/components/images/images.js
@@ -0,0 +1,11 @@
+const images ={
+    crudJdbc: require('./crud-jdbc.PNG').default,
+    dataStorage: require('./data-storage -MVC architecture.PNG').default,
+    hotelApp: require('./hotel-applicaiton.PNG').default,
+    springForm: require('./spring-form.PNG').default,
+    springThyme: require('./springboot-thymeleaf-application.PNG').default,
+    ticketSales: require('./Ticket-Sales-System.PNG').default,
+    
+}
+
+export default images;
